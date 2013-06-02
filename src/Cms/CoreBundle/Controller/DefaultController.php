fc178306a8968387d779d9536d2536f077670a41
bva100 <brianvanderson@gmail.com>
2013-06-01 23:46:00 -0700

    added assetManager as a service

--- src/Cms/CoreBundle/Controller/DefaultController.php
+++ src/Cms/CoreBundle/Controller/DefaultController.php
@@ -10,12 +10,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        $asset = $this->get('persister')->getRepo('CmsCoreBundle:Asset')->find('51aae72518a516340400000e');
-        $asset->removeOldestHistory();
-
-        $results = $this->get('persister')->save($asset);
-
-        $notices = $this->get('session')->getFlashBag()->get('notices');
-        echo '<pre>', \var_dump($results, $notices); die();
+        
     }
 }
