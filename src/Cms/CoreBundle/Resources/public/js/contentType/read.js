eac59e2c8e59d26c6a1c5d145861ecdc9f9a69a2
bva100 <brianvanderson@gmail.com>
2013-07-05 14:55:10 -0400

    dumped css and js files to prod

--- src/Cms/CoreBundle/Resources/public/js/contentType/read.js
+++ src/Cms/CoreBundle/Resources/public/js/contentType/read.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
     $('select').selectpicker();
     $(".select .dropdown-toggle").addClass('btn-info');
     $(".dropdown-menu").addClass('dropdown-inverse');
+    $(".dropdown-arrow").addClass('dropdown-arrow-inverse');
 });
