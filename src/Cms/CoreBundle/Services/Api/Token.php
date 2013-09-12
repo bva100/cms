3a4d0876408fbd5fc6e1d07d79ae55773463b24f
bva100 <brianvanderson@gmail.com>
2013-09-12 16:53:31 -0700

    upgraded rand and security for public access token in api service with mcrypt

--- src/Cms/CoreBundle/Services/Api/Token.php
+++ src/Cms/CoreBundle/Services/Api/Token.php
@@ -13,6 +13,9 @@ use \RuntimeException;
 class Token {
 
     const CURRENT_VERSION = '1';
+    const CYPHER = MCRYPT_RIJNDAEL_256;
+    const MODE   = MCRYPT_MODE_CBC;
+    const KEY    = 'Hs82Bsp1S9zw0e44uDn';
 
     private $token;
 
@@ -53,7 +56,7 @@ class Token {
      */
     public function setClientIdAndRawClientSecret($token)
     {
-        $token = $this->base64_url_decode($token);
+        $token = $this->decrypt($token);
         $this->setVersionWithToken($token);
         switch($this->version){
             case 1:
@@ -183,7 +186,8 @@ class Token {
         }
         switch($version){
             case '1':
-                $token = $this->base64_url_encode($clientId.':'.$clientSecret.':v='.self::CURRENT_VERSION);
+                $str = $clientId.':'.$clientSecret.':v='.self::CURRENT_VERSION;
+                $token = $this->encrypt($str);
                 break;
             default:
                 throw new RuntimeException('Unable to generate valid access token');
@@ -214,7 +218,45 @@ class Token {
         $val = $ary['int'] & 0x7FFFFFFF;   // 32-bit safe
         $fp = (float) $val / 2147483647.0; // convert to [0,1]
         $rand =  round($fp * $diff) + $min;
-        return hash('sha512', $rand);
+        return substr(hash('sha512', $rand), -15);
+    }
+
+    /**
+     * Encrypt token and use url base base64
+     *
+     * @param $plaintext
+     * @return string
+     */
+    public function encrypt($plaintext)
+    {
+        $td = mcrypt_module_open(self::CYPHER, '', self::MODE, '');
+        $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        mcrypt_generic_init($td, self::KEY, $iv);
+        $crypttext = mcrypt_generic($td, $plaintext);
+        mcrypt_generic_deinit($td);
+        return $this->base64_url_encode($iv.$crypttext);
+    }
+
+    /**
+     * Decrypt token from url base 64
+     *
+     * @param $crypttext
+     * @return string
+     */
+    public function decrypt($crypttext)
+    {
+        $crypttext = $this->base64_url_decode($crypttext);
+        $plaintext = '';
+        $td        = mcrypt_module_open(self::CYPHER, '', self::MODE, '');
+        $ivsize    = mcrypt_enc_get_iv_size($td);
+        $iv        = substr($crypttext, 0, $ivsize);
+        $crypttext = substr($crypttext, $ivsize);
+        if ($iv)
+        {
+            mcrypt_generic_init($td, self::KEY, $iv);
+            $plaintext = mdecrypt_generic($td, $crypttext);
+        }
+        return trim($plaintext);
     }
 
     /**
